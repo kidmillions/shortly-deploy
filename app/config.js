7251579e7e00ba07857724f4106260cde1fa3402
Hack Reactor Students <accounts@hackreactor.com>
2015-06-10 19:53:39 -0700

    begin refactoring into mongoose

--- app/config.js
+++ app/config.js
@@ -1,45 +1,60 @@
-var Bookshelf = require('bookshelf');
-var path = require('path');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shortly');
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback){
+
+var Schema = mongoose.Schema;
+
+  var urlSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    url: String,
+    base_url: String,
+    code: String,
+    title: String,
+    visits: Number,
+    created_At: {type: Date, default: Date.now},
+    //timestamp needed
+    _user: { type: Schema.Types.ObjectId, ref: 'User' }
+  })
+
+
+  var userSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    username: String,
+    password: String,
+    created_At: {type: Date, default: Date.now}
+  })
+
+  var Url = mongoose.model('Url', urlSchema);
+  var User = mongoose.model('User', userSchema);
+
+
+  // var url = new Url({url: 'www.me.com', base_url:'hi', code:'124354sfd', title:'I\'m working!', visits: 2142543});
+
+  // console.log(url.url);
+
+
+
+
 });
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+// var Bookshelf = require('bookshelf');
+// var path = require('path');
 
-module.exports = db;
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function (user) {
+//       user.increments('id').primary();
+//       user.string('username', 100).unique();
+//       user.string('password', 100);
+//       user.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+// module.exports = db;
